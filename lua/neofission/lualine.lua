b9de6bf8b3c4dc0457a700fd832c58a3ac0919a0
James Gregoire <jgregoire@ieee.org>
2025-01-13 15:26:20 -0500

    Visual uses faded_purple

--- lua/neofission/lualine.lua
+++ lua/neofission/lualine.lua
@@ -18,7 +18,7 @@ return {
         z = { bg = palette.light0, fg = palette.dark0, gui = "bold" },
     },
     visual = {
-        a = { bg = palette.neutral_purple, fg = palette.dark0, gui = "bold" },
+        a = { bg = palette.faded_purple, fg = palette.dark0, gui = "bold" },
         b = { bg = palette.neutral_blue, fg = palette.bright_aqua },
         c = { bg = palette.dark2, fg = palette.bright_aqua },
         x = { bg = palette.dark2, fg = palette.bright_aqua },
